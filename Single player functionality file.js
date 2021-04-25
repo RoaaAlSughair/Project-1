ecb537eef00dc626c3eb3803a4d7af8a409002ff
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-04-25 06:04:24 +0300

    Added the winning if statements. Still in need of work

--- Single player functionality file.js
+++ Single player functionality file.js
@@ -1,52 +1,125 @@
 const singlePlayer_body = document.querySelector("body");
 
-const roleChange =  () => {
-    
-    let roleSwitch = `X`;
+const roleChange = () => {
+  let roleSwitch = `X`;
 
-    return () => {
-      if (roleSwitch === `X`) {
-        roleSwitch = `O`;
-        return roleSwitch;
-      } else {
-        roleSwitch = `X`;
-        return roleSwitch;
-      }
-    };
+  return () => {
+    if (roleSwitch === `X`) {
+      roleSwitch = `O`;
+      return roleSwitch;
+    } else {
+      roleSwitch = `X`;
+      return roleSwitch;
+    }
   };
+};
 
-computerMove = () => {
-    squareNumber = Math.round(Math.round() * 9);
-}
+playerOneArray = [];
+playerTwoArray = [];
 
-const cards = document.querySelectorAll(".card")
+const cards = document.querySelectorAll(`.card`);
+
+const roleFlipper = roleChange();
+
 for (i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function() {
-        if(this.classList.contains('X') || this.classList.contains('O')){
-            alert("This square is occupied");
-        }else{
-            this.classList.add("X");
-        }
+  cards[i].addEventListener(`click`, function () {
+    let role = roleFlipper();
+
+    if (role === `O`) {
+      if (!this.classList.contains(`X`) && !this.classList.contains(`O`)) {
+        this.classList.add(`O`);
+        playerOneArray.push(this);
+
+        computerMove = () => {
+          let squareNumber = Math.round(Math.round() * 9);
+
+          if (
+            !document.getElementById(`${squareNumber}`).contains(`X`) &&
+            !document.getElementById(`${squareNumber}`).contains(`O`)
+          ) {
+            document.getElementById(`${squareNumber}`).classList.add(`X`);
+            playerTwoArray.push(document.getElementById(`${squareNumber}`));
+          } else {
+            return computerMove();
+          }
+        };
+      } else {
+        alert(`This square is occupied`);
+      }
+    }
   });
 }
 
+winningArray = [
+  [
+    document.getElementById(`1`),
+    document.getElementById(`2`),
+    document.getElementById(`3`),
+  ],
+  [
+    document.getElementById(`4`),
+    document.getElementById(`5`),
+    document.getElementById(`6`),
+  ],
+  [
+    document.getElementById(`7`),
+    document.getElementById(`8`),
+    document.getElementById(`9`),
+  ],
+  [
+    document.getElementById(`1`),
+    document.getElementById(`4`),
+    document.getElementById(`7`),
+  ],
+  [
+    document.getElementById(`2`),
+    document.getElementById(`5`),
+    document.getElementById(`8`),
+  ],
+  [
+    document.getElementById(`3`),
+    document.getElementById(`6`),
+    document.getElementById(`9`),
+  ],
+  [
+    document.getElementById(`1`),
+    document.getElementById(`5`),
+    document.getElementById(`9`),
+  ],
+  [
+    document.getElementById(`3`),
+    document.getElementById(`5`),
+    document.getElementById(`7`),
+  ],
+];
 
+const compareArrays = (array1, array2) => {
+  let comparisonArray = [];
 
-// const cards = document.querySelectorAll(`.card`)
-// const roleFlipper = roleChange();
-// for (i = 0; i < cards.length; i++) {
-//     cards[i].addEventListener(`click`, function() {
-//         if(!this.classList.contains(`X`) && !this.classList.contains(`O`)){
+  for (i = 0, j = 0; i < array1.length && j < array2.length; i++, j++) {
+    if (array1[i] === array2[j]) {
+      comparisonArray.push(array1[i]);
+    }
+    return comparisonArray;
+  }
+};
 
-//             let role = roleFlipper();
+if (playerOneArray.length >= 3) {
+  playerOneArray.sort();
 
-//             if (role === `X`) {
-//                 this.classList.add(`X`);
-//             } else if (role === `O`) {
-//                 this.classList.add(`O`);
-//             }
-//         } else {
-//             alert(`This square is occupied`);
-//         }
-//   });
-// };
+  winningArray.array.forEach((element) => {
+    if (compareArrays(playerOneArray, element).length === element.length) {
+      alert(`Winner: Player wins`);
+    }
+  });
+}
+
+if (playerTwoArray.length >= 3) {
+  playerTwoArray.sort();
+
+  winningArray.array.forEach((element) => {
+    if (compareArrays(playerTwoArray, element).length === element.length) {
+      alert(`Winner: Computer wins`);
+    }
+  });
+}
--- Two players functionality file.js
+++ Two players functionality file.js
@@ -1,34 +1,114 @@
 const twoPlayers_body = document.querySelector(`body`);
 
-const roleChange =  () => {
-    let roleSwitch = `X`;
+const roleChange = () => {
+  let roleSwitch = `X`;
 
-    return () => {
-      if (roleSwitch === `X`) {
-        roleSwitch = `O`;
-        return roleSwitch;
-      } else {
-        roleSwitch = `X`;
-        return roleSwitch;
-      }
-    };
+  return () => {
+    if (roleSwitch === `X`) {
+      roleSwitch = `O`;
+      return roleSwitch;
+    } else {
+      roleSwitch = `X`;
+      return roleSwitch;
+    }
   };
+};
 
-const cards = document.querySelectorAll(`.card`)
+playerOneArray = [];
+playerTwoArray = [];
+
+const cards = document.querySelectorAll(`.card`);
+
 const roleFlipper = roleChange();
+
 for (i = 0; i < cards.length; i++) {
-    cards[i].addEventListener(`click`, function() {
-        if(!this.classList.contains(`X`) && !this.classList.contains(`O`)){
+  cards[i].addEventListener(`click`, function () {
+    if (!this.classList.contains(`X`) && !this.classList.contains(`O`)) {
+      let role = roleFlipper();
 
-            let role = roleFlipper();
+      if (role === `X`) {
+        this.classList.add(`X`);
+        playerTwoArray.push(this);
+      } else if (role === `O`) {
+        this.classList.add(`O`);
+        playerOneArray.push(this);
+      }
+    } else {
+      alert(`This square is occupied`);
+    }
+  });
+}
 
-            if (role === `X`) {
-                this.classList.add(`X`);
-            } else if (role === `O`) {
-                this.classList.add(`O`);
-            }
-        } else {
-            alert(`This square is occupied`);
-        }
+winningArray = [
+  [
+    document.getElementById(`1`),
+    document.getElementById(`2`),
+    document.getElementById(`3`),
+  ],
+  [
+    document.getElementById(`4`),
+    document.getElementById(`5`),
+    document.getElementById(`6`),
+  ],
+  [
+    document.getElementById(`7`),
+    document.getElementById(`8`),
+    document.getElementById(`9`),
+  ],
+  [
+    document.getElementById(`1`),
+    document.getElementById(`4`),
+    document.getElementById(`7`),
+  ],
+  [
+    document.getElementById(`2`),
+    document.getElementById(`5`),
+    document.getElementById(`8`),
+  ],
+  [
+    document.getElementById(`3`),
+    document.getElementById(`6`),
+    document.getElementById(`9`),
+  ],
+  [
+    document.getElementById(`1`),
+    document.getElementById(`5`),
+    document.getElementById(`9`),
+  ],
+  [
+    document.getElementById(`3`),
+    document.getElementById(`5`),
+    document.getElementById(`7`),
+  ],
+];
+
+const compareArrays = (array1, array2) => {
+  let comparisonArray = [];
+
+  for (i = 0, j = 0; i < array1.length && j < array2.length; i++, j++) {
+    if (array1[i] === array2[j]) {
+      comparisonArray.push(array1[i]);
+    }
+    return comparisonArray;
+  }
+};
+
+if (playerOneArray.length >= 3) {
+  playerOneArray.sort();
+
+  winningArray.array.forEach((element) => {
+    if (compareArrays(playerOneArray, element).length === element.length) {
+      alert(`Winner: Player 1 wins`);
+    }
+  });
+}
+
+if (playerTwoArray.length >= 3) {
+  playerTwoArray.sort();
+
+  winningArray.array.forEach((element) => {
+    if (compareArrays(playerTwoArray, element).length === element.length) {
+      alert(`Winner: Player 2 wins`);
+    }
   });
 }
