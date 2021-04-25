33216a4092fa01d65507858641340ed83d5157fb
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-04-26 00:49:41 +0300

    I've been having some technical issues and I'm trying to resolve them

--- Single player functionality file.js
+++ Single player functionality file.js
@@ -25,6 +25,7 @@ const cards = document.querySelectorAll(`.card`);
 //   }
 // };
 
+
 for (i = 0; i < cards.length; i++) {
   cards[i].addEventListener(`click`, () => {
     if (!this.classList.contains(`X`) && !this.classList.contains(`O`)) {
